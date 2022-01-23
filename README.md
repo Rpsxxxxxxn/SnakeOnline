83e170741e4eabf8891c131fc6093fa3cd1dadb8
Rpsxxxxxxn <keito0122.y@gmail.com>
2022-01-23 09:36:17 +0900

    webの追加

---
+++ README.md
@@ -0,0 +1,2 @@
+# へびへび
+websocket
--- work/main.go
+++ work/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/net/websocket"
+)
 
 // メインエントリ
 func main() {
